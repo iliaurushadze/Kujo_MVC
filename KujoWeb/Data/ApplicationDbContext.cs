7b07bfc44dfeb7dd7653aa47128b8d0f27c7d861
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-13 14:05:07 +0400

    Added ApplicationDbContext.

---
+++ KujoWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using KujoWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KujoWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
