47de242e4a411938c92b59a2a3c1666f560f29f0
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-13 21:07:26 +0400

    Added Edit Category GET.

--- KujoWeb/Controllers/CategoryController.cs
+++ KujoWeb/Controllers/CategoryController.cs
@@ -17,6 +17,9 @@ namespace KujoWeb.Controllers
             
             return View(objCategoryList);
         }
+
+        //_______________________________________________________________
+
         //GET
         public IActionResult Create()
         {  
@@ -39,5 +42,43 @@ namespace KujoWeb.Controllers
             }
             return View(obj);
         }
+
+        //_______________________________________________________________
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exatly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //_______________________________________________________________
     }
 }
