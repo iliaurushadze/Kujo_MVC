513c437afb10f255dace3fe49b6650219550aba9
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-13 15:30:18 +0400

    Retrieved Categories From DB.

--- KujoWeb/Controllers/CategoryController.cs
+++ KujoWeb/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KujoWeb.Data;
+using KujoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KujoWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
+            
             return View();
         }
     }
