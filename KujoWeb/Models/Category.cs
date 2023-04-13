6dda7b65d195f9d96d68e52b53c563301cc3c214
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-13 19:57:14 +0400

    Added Display Name and Range Validations.

--- KujoWeb/Models/Category.cs
+++ KujoWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace KujoWeb.Models
 {
@@ -6,9 +7,14 @@ namespace KujoWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 200, ErrorMessage ="Display Order Must Be between 1 And 100 Only!!")]
         public int DisplayOrder { get; set; }
+
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
